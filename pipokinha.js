9da5b5ebe357c6de9493779c7fd918f35acc6336
natyyp <natypiresoliveira@gmail.com>
2023-05-10 17:06:16 -0300

    pipokinha

---
+++ pipokinha.js
@@ -0,0 +1,84 @@
+// const filme = {
+// 	nome: "Auto da Compadecida", 
+// 	ano: 2000, 
+// 	elenco: [
+// 		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+// 		"Virginia Cavendish"
+// 		], 
+// 	transmissoesHoje: [
+// 		{canal: "Telecine", horario: "21h"}, 
+// 		{canal: "Canal Brasil", horario: "19h"}, 
+// 		{canal: "Globo", horario: "14h"}
+// 		]
+// }
+
+// console.log(filme.elenco[0])
+// console.log(filme.elenco[filme.elenco.length - 1])
+// console.log(filme.transmissoesHoje[2])
+
+
+// const cachorro = {
+// 	nome: "Juca", 
+// 	idade: 3, 
+// 	raca: "SRD"
+// }
+
+// const gato = {...cachorro, nome: "Juba"}
+
+// const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+// console.log(cachorro)
+// console.log(gato)
+// console.log(tartaruga)
+
+
+// function minhaFuncao(objeto, propriedade) {
+// 	return objeto[propriedade]
+// }
+
+// const pessoa = {
+//   nome: "Caio", 
+//   idade: 23, 
+//   backender: false
+// }
+
+// console.log(minhaFuncao(pessoa, "backender"))
+// console.log(minhaFuncao(pessoa, "altura"))
+
+
+// const vito = {
+//     nome:"vitor",
+//     apelidos:"vitu,vito,vitin"
+// }
+
+// const novovito = {
+//     ...vito
+// }
+
+
+// function vito2(vitin){
+//  console.log(`Olá meu nome é ${vito.nome}, sou conhecido como: ${vito.apelidos}`)
+//  }
+
+// vito2(vito)
+
+// vito.apelidos = "vitom, varudo, pingolamucha"
+// vito2(vito)
+
+
+
+// const usario ={
+//  nome :"vito",
+//  idade:"16",
+//  profissao:"prostituta"
+// }  
+
+// function fumsao(usario){
+//    const nome = usario.nome
+//    const caracternome = nome.length
+//    const idade = usario.idade
+//    const profissao = usario.profissao
+//    const caraterprofisao = profissao.length
+//    return[nome,caracternome,idade,profissao,caraterprofisao]
+// }
+// console.log(fumsao(usario))
